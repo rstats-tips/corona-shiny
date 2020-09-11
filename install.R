d11f9106656c2e051c570f9f1f961ecd755f8d81
rstats <rstats.tips@gmail.com>
2020-09-11 14:28:23 +0200

    Try and Error: binder

--- install.R
+++ install.R
@@ -1,5 +1,14 @@
 # Basics
-install.packages(c("tidyverse", "lubridate", "scales", "rlang", "plotly", "zoo", "DT", "plotly", "leaflet", "geojsonio"))
+install.packages("tidyverse")
+install.packages("lubridate")
+install.packages("scales")
+install.packages("rlang")
+install.packages("plotly")
+install.packages("zoo")
+install.packages("DT")
+install.packages("plotly")
+install.packages("leaflet")
+install.packages("geojsonio")
 
 # Shiny
 install.packages("shiny")
